f66208462dbbdfcd435e671a4728460444713967
Stephen Thoni <stephen.thoni@gmail.com>
2022-04-18 16:33:39 -0500

    image picker opening

--- FanMaker/src/main/java/com/fanmaker/sdk/FanMakerSDKWebView.kt
+++ FanMaker/src/main/java/com/fanmaker/sdk/FanMakerSDKWebView.kt
@@ -1,14 +1,16 @@
 package com.fanmaker.sdk
 
 import android.Manifest
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.webkit.PermissionRequest
-import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.android.volley.Request
@@ -22,7 +24,11 @@ class FanMakerSDKWebView : AppCompatActivity() {
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.READ_EXTERNAL_STORAGE,
     )
-    private val requestCode = 1
+    private val FILECHOOSER_RESULTCODE = 1
+    private val REQUEST_SELECT_FILE = 100
+
+    private var mUploadMessage: ValueCallback<Uri>? = null
+    private var uploadMessage: ValueCallback<Array<Uri>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +38,6 @@ class FanMakerSDKWebView : AppCompatActivity() {
         webView.webViewClient = WebViewClient()
         webView.webChromeClient = WebChromeClient()
 
-        webView.settings.javaScriptEnabled = true
         webView.settings.javaScriptEnabled = true
         webView.settings.javaScriptCanOpenWindowsAutomatically = true
         webView.settings.domStorageEnabled = true
@@ -44,6 +49,38 @@ class FanMakerSDKWebView : AppCompatActivity() {
         webView.webChromeClient = object : WebChromeClient() {
             override fun onPermissionRequest(request: PermissionRequest?) {
                 request?.grant(request?.resources)
+            }
+
+            private fun openFileChooser(uploadMsg: ValueCallback<Uri>) {
+                mUploadMessage = uploadMsg
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.type = "*/*"
+                startActivityForResult(Intent.createChooser(intent, "File Chooser"), FILECHOOSER_RESULTCODE)
+            }
+
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                uploadMessage?.onReceiveValue(null)
+                uploadMessage = null
+
+                uploadMessage = filePathCallback
+
+                val intent = fileChooserParams!!.createIntent()
+                try {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE)
+                } catch (e: ActivityNotFoundException) {
+                    uploadMessage = null
+                    Toast.makeText(applicationContext, "Cannot Open File Chooser", Toast.LENGTH_LONG).show()
+                    return false
+                }
+
+                return true
+
+//                return super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
             }
         }
 
@@ -115,7 +152,7 @@ class FanMakerSDKWebView : AppCompatActivity() {
     }
 
     private fun askPermissions() {
-        ActivityCompat.requestPermissions(this, permission, requestCode)
+        ActivityCompat.requestPermissions(this, permission, FILECHOOSER_RESULTCODE)
     }
 
     private fun isPermissionGranted(): Boolean {
@@ -124,5 +161,29 @@ class FanMakerSDKWebView : AppCompatActivity() {
                 return false
         }
         return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode === REQUEST_SELECT_FILE) {
+                if (uploadMessage == null)
+                    return
+                print("result code = " + resultCode)
+                var results: Array<Uri>? = WebChromeClient.FileChooserParams.parseResult(resultCode, data)
+                uploadMessage?.onReceiveValue(results)
+                uploadMessage = null
+            }
+        } else if (requestCode === FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            val result = if (intent == null || resultCode !== RESULT_OK) null else intent.data
+            mUploadMessage?.onReceiveValue(result)
+            mUploadMessage = null
+        } else
+            Toast.makeText(applicationContext, "Failed to Upload Image", Toast.LENGTH_LONG).show()
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
