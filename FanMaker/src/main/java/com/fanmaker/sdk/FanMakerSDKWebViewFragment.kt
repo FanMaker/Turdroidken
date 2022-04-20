545f0eaa1f16735fe82c42d71a279e1069ab88dc
Stephen Thoni <stephen.thoni@gmail.com>
2022-04-20 14:37:58 -0500

    ported camrea methods to fragment

--- FanMaker/src/main/java/com/fanmaker/sdk/FanMakerSDKWebViewFragment.kt
+++ FanMaker/src/main/java/com/fanmaker/sdk/FanMakerSDKWebViewFragment.kt
@@ -13,13 +13,168 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Camera
+import android.net.Uri
+import android.provider.MediaStore
+import android.webkit.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+
 class FanMakerSDKWebViewFragment : Fragment() {
+    private val permission = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+    )
+    private val MEDIA_RESULTCODE = 1
+    private val PERMISSION_RESULTCODE = 2
+    private val REQUEST_SELECT_FILE = 100
+
+    private var mUploadMessage: ValueCallback<Uri>? = null
+    private var uploadMessage: ValueCallback<Array<Uri>>? = null
+    private var imagePath: String? = null;
+    private lateinit var photoURI : Uri;
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val fragmentView = inflater.inflate(R.layout.fanmaker_sdk_webview_fragment, container, false)
 
         val webView = fragmentView.findViewById<WebView>(R.id.fanmaker_sdk_webview)
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
         webView.settings.javaScriptEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowContentAccess = true
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+
+        if(!isPermissionGranted()) { askPermissions() }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                request?.grant(request?.resources)
+            }
+
+            private fun openFileChooser(uploadMsg: ValueCallback<Uri>) {
+                mUploadMessage = uploadMsg
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.type = "*/*"
+                startActivityForResult(Intent.createChooser(intent, "File Chooser"), MEDIA_RESULTCODE)
+            }
+
+            private fun createFile(): File {
+                val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                val storageDir: File? = activity?.filesDir
+                return File.createTempFile(
+                    "JPEG_${timestamp}_",
+                    ".jpg",
+                    storageDir
+                ).apply {
+                    imagePath = absolutePath
+                }
+            }
+
+            fun takePhoto() {
+                val hasCamPermissions = (ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.CAMERA) ==
+                        PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(context!!,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+
+                if(hasCamPermissions) {
+                    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+                            takePictureIntent -> takePictureIntent.resolveActivity(context!!.packageManager)
+                        if(takePictureIntent != null) {
+                            val photo: File = createFile()
+                            photo?.also {
+                                photoURI = FileProvider.getUriForFile(context!!, "com.fanmaker.sdk.fileprovider", it)
+                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                                takePictureIntent.putExtra("android.intent.extras.CAMERA_FACING", Camera.CameraInfo.CAMERA_FACING_FRONT);
+                                startActivityForResult(takePictureIntent, MEDIA_RESULTCODE)
+                            }
+                        }
+                    }
+                } else {
+                    genericAlert("Please make sure the app has access to your Camera and Media Gallery.")
+                    uploadMessage?.onReceiveValue(null)
+                    uploadMessage = null
+                }
+            }
+
+            fun genericAlert(message: String) {
+                val dialogBuilder = AlertDialog.Builder(context)
+
+                dialogBuilder.setTitle("Unable to complete your request")
+                dialogBuilder.setMessage(message)
+                dialogBuilder.setCancelable(false)
+                dialogBuilder.setNegativeButton("Close", DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+                var alert = dialogBuilder.create()
+                alert.show()
+            }
+
+            fun openPicker(fileChooserParams: FileChooserParams?) {
+                val hasFilePermissions = (ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+
+                if(hasFilePermissions) {
+                    val intent = fileChooserParams!!.createIntent()
+                    try {
+                        startActivityForResult(intent, REQUEST_SELECT_FILE)
+                    } catch (e: ActivityNotFoundException) {
+                        uploadMessage = null
+                        Toast.makeText(context, "Cannot Open File Picker", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    genericAlert("Please make sure the app has access to your Media Gallery")
+                    uploadMessage?.onReceiveValue(null)
+                    uploadMessage = null
+                }
+            }
+
+            private fun selectImage(fileChooserParams: FileChooserParams?) {
+                val dialogBuilder = AlertDialog.Builder(context)
+
+                dialogBuilder.setTitle("Complete action using?")
+                dialogBuilder.setCancelable(false)
+                dialogBuilder.setPositiveButton("Camera", DialogInterface.OnClickListener { dialog, id ->
+                    takePhoto()
+                    dialog.cancel()
+                })
+                dialogBuilder.setNegativeButton("Gallery", DialogInterface.OnClickListener { dialog, id ->
+                    openPicker(fileChooserParams)
+                    dialog.cancel()
+                })
+
+                var alert = dialogBuilder.create()
+                alert.show()
+            }
+
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                uploadMessage?.onReceiveValue(null)
+                uploadMessage = null
+                uploadMessage = filePathCallback
+
+                selectImage(fileChooserParams)
+                return true
+            }
+        }
+
         val jsInterface = FanMakerSDKWebInterface(requireActivity(),
             { authorized ->
                 var jsString: String = "FanMakerReceiveLocationAuthorization("
@@ -88,5 +243,43 @@ class FanMakerSDKWebViewFragment : Fragment() {
         queue.add(request)
 
         return fragmentView
+    }
+
+    private fun askPermissions() {
+        ActivityCompat.requestPermissions(requireActivity(), permission, PERMISSION_RESULTCODE)
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        permission.forEach {
+            if(ActivityCompat.checkSelfPermission(requireActivity(), it) != PackageManager.PERMISSION_GRANTED)
+                return false
+        }
+        return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MEDIA_RESULTCODE) {
+            if(resultCode == Activity.RESULT_OK) {
+                Log.w("PHOTO URI", photoURI.toString())
+                var results: Array<Uri>? = arrayOf(photoURI)
+
+                uploadMessage?.onReceiveValue(results)
+                uploadMessage = null
+            } else {
+                uploadMessage?.onReceiveValue(null)
+                uploadMessage = null
+            }
+        } else if(requestCode == REQUEST_SELECT_FILE) {
+            Log.w("PICKER RESULTS", data.toString())
+            if (uploadMessage == null)
+                return
+            var results: Array<Uri>? = WebChromeClient.FileChooserParams.parseResult(resultCode, data)
+            Log.w("SELECTED IMAGE DATA", data.toString())
+            Log.w("SELECTED IMAGE PATH", WebChromeClient.FileChooserParams.parseResult(resultCode, data).toString())
+            Log.w("SELECTED IMAGE RESULTS", results.toString())
+            uploadMessage?.onReceiveValue(results)
+            uploadMessage = null
+        }
     }
 }
